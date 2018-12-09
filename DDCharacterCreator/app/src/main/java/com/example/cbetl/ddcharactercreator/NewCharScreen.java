6528857cb74432ea408824ff990ca846e1395398
Codie Betlinski <cbetlinski98@gmail.com>
2018-12-09 17:37:08 -0600

    Final Project

--- DDCharacterCreator/app/src/main/java/com/example/cbetl/ddcharactercreator/NewCharScreen.java
+++ DDCharacterCreator/app/src/main/java/com/example/cbetl/ddcharactercreator/NewCharScreen.java
@@ -5,19 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class NewCharScreen extends AppCompatActivity {
+
+    private String charName = "";
+    private String charClass = "";
+    private String charRace = "";
+    private String charStr = "";
+    private String charDex = "";
+    private String charCon = "";
+    private String charInt = "";
+    private String charWis = "";
+    private String charCha = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_char_screen);
 
+        Bundle extras = getIntent().getExtras();
+        if (extras!=null){
+            charName = extras.getString("CharacterName");
+            charClass = extras.getString("CharacterClass");
+            charRace = extras.getString("CharacterRace");
+            charStr = extras.getString("CharacterStrength");
+            charDex = extras.getString("CharacterDexterity");
+            charCon = extras.getString("CharacterConstitution");
+            charInt = extras.getString("CharacterIntelligence");
+            charWis = extras.getString("CharacterWisdom");
+            charCha = extras.getString("CharacterCharisma");
+        }
+
         Button btnSave = (Button)findViewById(R.id.btnSaveChar);
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(NewCharScreen.this, HomeScreen.class));
+                if (checkData())
+                    startActivity(new Intent(NewCharScreen.this, HomeScreen.class));
             }
         });
 
@@ -28,5 +53,17 @@ public class NewCharScreen extends AppCompatActivity {
                 startActivity(new Intent(NewCharScreen.this, ExplanationScreen.class));
             }
         });
+    }
+
+    private boolean checkData(){
+        final EditText charName = (EditText) findViewById(R.id.txtNameInput);
+
+        if (charName.getText().toString().isEmpty()){
+            charName.setError("Enter character name");
+            charName.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 }
